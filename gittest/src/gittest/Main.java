d5588844574fbc41dcf51218e72d281ac8df0c96
smhrc10 <smhrc10@DESKTOP-6SK703I>
2018-05-18 10:37:08 +0900

    333

--- gittest/src/gittest/Main.java
+++ gittest/src/gittest/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("안녕하신지요");
-		System.out.println("안녕하신지요");
+		System.out.println("안녕하신지요222");
 	}
 
 }
