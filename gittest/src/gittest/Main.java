f1d5b8ecc4b0c3fe6600685847be62daa1736648
smhrc20 <smhrc20@DESKTOP-F9CM58T>
2018-05-18 10:37:50 +0900

    # WARNING: head commit changed in the meantime
    안녕하신지요22

--- gittest/src/gittest/Main.java
+++ gittest/src/gittest/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("안녕하신지요");
+		System.out.println("안녕하신지요222");
 		System.out.println("안녕하신지요");
 	}
 
